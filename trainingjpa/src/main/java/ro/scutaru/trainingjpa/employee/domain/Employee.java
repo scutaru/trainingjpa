540470b406fdafaa8823649e1eb45165c1c71574
Ionut Scutaru <ionut.scutaru@gmail.com>
2013-11-15 13:22:59 +0200

    Removed a warning from the Employee class.

--- trainingjpa/src/main/java/ro/scutaru/trainingjpa/employee/domain/Employee.java
+++ trainingjpa/src/main/java/ro/scutaru/trainingjpa/employee/domain/Employee.java
@@ -10,7 +10,6 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.PostPersist;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
